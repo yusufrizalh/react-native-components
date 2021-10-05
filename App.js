d5615618df3e80457a34197d37768a67e7fbcf9b
yusufrizalh <yusufrizalh@gmail.com>
2021-10-05 15:03:09 +0700

    react native alert and modal

--- App.js
+++ App.js
@@ -1,10 +1,69 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Button,
+  Alert,
+  TouchableOpacity,
+  Modal,
+  Image,
+} from "react-native";
 
 class ViewComponent extends Component {
   constructor() {
     super();
+    this.state = {
+      isVisible: false,
+    };
   }
+
+  bukaModal(show) {
+    this.setState({ isVisible: show });
+  }
+
+  bukaAlert = () => {
+    alert("Alert dengan satu tombol");
+  };
+
+  bukaAlert2Tombol = () => {
+    Alert.alert(
+      "Hello!",
+      "Alert dengan dua tombol",
+      [
+        { text: "Ok", onPress: () => console.log("Menekan tombol Ok") },
+        {
+          text: "Cancel",
+          onPress: () => console.log("Menekan tombol cancel"),
+          style: "cancel",
+        },
+      ],
+      {
+        cancelable: true,
+      }
+    );
+  };
+
+  bukaAlert3Tombol = () => {
+    Alert.alert(
+      "Hello!",
+      "Alert dengan tiga tombol",
+      [
+        { text: "Ok", onPress: () => console.log("Menekan tombol Ok") },
+        {
+          text: "Cancel",
+          onPress: () => console.log("Menekan tombol cancel"),
+        },
+        {
+          text: "Ask me later",
+          onPress: () => console.log("Menekan tombol ask me later"),
+        },
+      ],
+      {
+        cancelable: false,
+      }
+    );
+  };
 
   render() {
     return (
@@ -13,14 +72,65 @@ class ViewComponent extends Component {
           <Text style={myStyles.teksBiasa}>Header</Text>
         </View>
         <View style={myStyles.wadahKecil2}>
-          {/* <Text style={myStyles.teksBiasa}>Content</Text> */}
+          <View style={myStyles.wadahModal}>
+            <Modal
+              visible={this.state.isVisible}
+              animationType={"slide"}
+              transparent={false}
+              onRequestClose={() => {
+                Alert.alert("Modal ditutup!");
+              }}
+            >
+              <Image
+                style={myStyles.gambar}
+                source={require("./assets/image_1.jpg")}
+              />
+              <Text style={myStyles.teksWarna}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec
+                justo in ipsum sodales eleifend. Vestibulum ante ipsum primis in
+                faucibus orci luctus et ultrices posuere cubilia curae;
+              </Text>
+              <Text> </Text>
+              <TouchableOpacity
+                style={myStyles.wadahTouchable}
+                onPress={() => {
+                  this.bukaModal(!this.state.isVisible);
+                }}
+              >
+                <Text style={myStyles.teksBiasa}>Close Modal</Text>
+              </TouchableOpacity>
+            </Modal>
+          </View>
+
           <View style={myStyles.wadahButton}>
             <Button title="Click Me" color="#005b96" />
           </View>
           <View style={myStyles.wadahBanyakButton}>
-            <Button title="Button One" color="#005b96" />
-            <Button title="Button Two" color="#005b96" />
-            <Button title="Button Three" color="#005b96" />
+            <Button
+              title="Button One"
+              color="#005b96"
+              onPress={this.bukaAlert}
+            />
+            <Button
+              title="Button Two"
+              color="#005b96"
+              onPress={this.bukaAlert2Tombol}
+            />
+            <Button
+              title="Button Three"
+              color="#005b96"
+              onPress={this.bukaAlert3Tombol}
+            />
+          </View>
+          <View style={myStyles.wadahButton}>
+            <TouchableOpacity
+              style={myStyles.wadahTouchable}
+              onPress={() => {
+                this.bukaModal(true);
+              }}
+            >
+              <Text style={myStyles.teksBiasa}>Buka Modal</Text>
+            </TouchableOpacity>
           </View>
         </View>
         <View style={myStyles.wadahKecil3}>
@@ -65,6 +175,11 @@ const myStyles = StyleSheet.create({
     color: "#ffffff",
     textAlign: "center",
   },
+  teksWarna: {
+    fontSize: 18,
+    color: "#005b96",
+    textAlign: "center",
+  },
   wadahButton: {
     margin: 16,
   },
@@ -72,5 +187,29 @@ const myStyles = StyleSheet.create({
     margin: 16,
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  wadahTouchable: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
+    height: 60,
+    width: "100%",
+    borderRadius: 10,
+    backgroundColor: "#005b96",
+    shadowOpacity: 0.8,
+    shadowRadius: 24,
+  },
+  wadahModal: {
+    flex: 1,
+    padding: 24,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  gambar: {
+    margin: 18,
+    padding: 18,
+    width: "90%",
+    height: 350,
   },
 });
